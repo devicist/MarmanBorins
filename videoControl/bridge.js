62a78e31c432bf1ebd297ccb6f8c64ea1a8f30c0
devlinmacpherson <dev.macpherson@gmail.com>
2023-02-02 13:19:22 -0500

    upload

---
+++ videoControl/bridge.js
@@ -0,0 +1,37 @@
+// https://github.com/stephanschulz/p5js-osc
+
+var osc = require("node-osc");
+var io = require("socket.io")(8081);
+
+var oscServer, oscClient;
+
+var isConnected = false;
+
+console.log("bridge.js running. Waiting for connection...");
+io.sockets.on("connection", function (socket) {
+  console.log("connection");
+  socket.on("config", function (obj) {
+    isConnected = true;
+    oscServer = new osc.Server(obj.server.port, obj.server.host);
+    oscClient = new osc.Client(obj.client.host, obj.client.port);
+    oscClient.send("/status", socket.sessionId + " connected");
+    oscServer.on("message", function (msg, rinfo) {
+      socket.emit("message", msg);
+    });
+    socket.emit("connected", 1);
+  });
+  // 	socket.on("message", function (obj) {
+  //		oscClient.send.apply(oscClient, obj);
+  //  	});
+  socket.on("message", function (address, value) {
+    console.log(address + ":" + value);
+    oscClient.send(address, value);
+  });
+  socket.on("disconnect", function () {
+    if (isConnected) {
+      console.log("disconnect");
+      oscServer.kill();
+      oscClient.kill();
+    }
+  });
+});
